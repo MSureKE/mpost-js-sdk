d3fdc8e73a9b754c0e24c00494be225db8b1bc18
ekbaya <Eliasbaya1223@gmail.com>
2022-08-03 12:00:44 +0300

    Removed all files that are in the .gitignore

--- mpost-js-sdk/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Base } from "./base";
-import { DeliveryRequests } from "./deliveryRequests";
-declare class Mpost extends Base {
-}
-interface Mpost extends DeliveryRequests {
-}
-export default Mpost;
