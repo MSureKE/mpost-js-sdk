4368b2c9e14ec74115fa8579e23f763b59e0b248
ekbaya <Eliasbaya1223@gmail.com>
2022-08-04 10:52:01 +0300

    Removed all files that are in the .gitignore

--- mpost-js-sdk-example/node_modules/@ekbaya/mpost-js-sdk/dist/deliveryRequests/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Base } from "../base";
-import { NewDeliveryRequest, DeliveryRequest } from "../models/delivery-request";
-import { DistanceRequest, Distance } from "../models/distance";
-export declare class DeliveryRequests extends Base {
-    getDeliveryRequestById(id: number): Promise<DeliveryRequest>;
-    getDeliveryRequests(): Promise<DeliveryRequest[]>;
-    createDeliveryRequest(newPost: NewDeliveryRequest): Promise<DeliveryRequest>;
-    calculateDistance(newPost: DistanceRequest): Promise<Distance>;
-}
